d9aa2902c7b32314d5981c5227022b7d347e03af
Eric Charnesky <eric.charnesky@careevolution.com>
2017-02-09 16:53:55 -0500

    custom arraylist

---
+++ Arrays/Arrays/ArrayList.py
@@ -0,0 +1,55 @@
+import ctypes
+
+class ArrayList(object):
+    """Custom implemenation of an 'array' backed list"""
+
+    def __init__(self, capacity=1):
+        self._size = 0
+        if capacity <= 0:
+            capacity = 1
+        self._capacity = capacity
+        self._storage = self._make_array(self._capacity)
+
+    def __len__(self):
+        return self._size
+
+    def __getitem__(self, index):
+        if not 0 <= index <= self._size:
+            raise IndexError()
+        return self._storage[index]
+    
+    def append(self, obj):
+        if self._size == self._capacity:
+            self._resize( 2 * self._capacity )
+        self._storage[self._size] = obj
+        self._size += 1
+
+    def pop(self):
+        self._size -= 1
+        temp = self._storage[ self._size ] 
+        self._storage[ self._size ] = None
+        return temp
+
+    def remove(self, obj):
+        item_was_at_index = -1
+        for index in range(self._size):
+            if self._storage[index] == obj:
+                self._storage[index] = None
+                item_was_at_index = index
+                break
+        if item_was_at_index != -1:
+            for index in range(item_was_at_index, self._size - 1):
+                self._storage[index] = self._storage[index + 1]
+            self._storage[self._size - 1] = None
+            self._size -= 1
+        return item_was_at_index != -1
+
+    def _resize(self, new_capacity):
+        new_storage = self._make_array(new_capacity)
+        for index in range(self._size):
+            new_storage[index] = self._storage[index]
+        self._storage = new_storage
+        self._capacity = new_capacity
+
+    def _make_array(self, capacity):
+        return ( capacity * ctypes.py_object)()
--- Arrays/Arrays/Arrays.py
+++ Arrays/Arrays/Arrays.py
@@ -1,5 +1,7 @@
 import array
 
+from ArrayList import ArrayList
+
 names = []
 names.append("Eric")
 names.append("Robert")
@@ -12,8 +14,27 @@ print(names)
 print(names[0])
 
 # 8 bytes per item for memoryt location + 4 bytes for each int
-list_primes = [2,3,5,7,11,13,17,19]
+list_primes = [2,3,5,7,11,13,17,19,2]
+print(list_primes)
+list_primes.remove(2)
+print(list_primes)
+
 # 4 bytes per int
 array_primes = array.array( 'i', list_primes)
 array_primes.append(23)
 print(array_primes)
+
+our_list = ArrayList()
+our_list.append("Eric")
+our_list.append("Robert")
+our_list.append("Fatemeh")
+our_list.append("Susan")
+our_list.append("Aya")
+our_list.append("Bryant")
+
+our_list.remove("Aya")
+our_list.remove("Bryant")
+
+for index in range(len(our_list)):
+    print(our_list[index])
+
